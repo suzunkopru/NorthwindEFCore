cc3411b55b3338678fb17d525ef6c0dfc832230b
Süleyman UZUNKÖPRÜ <s-uzunkopru@yandex.com>
2023-01-02 17:34:21 +0300

    UIBlazor Projesi

--- UIBlazor/Program.cs
+++ UIBlazor/Program.cs
@@ -7,8 +7,8 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddDbContext<NorthwindContext>();
-builder.Services.AddScoped<DalCategory>();
-builder.Services.AddScoped<DalProduct>();
+builder.Services.AddScoped<IDalCategory, DalCategory>();
+builder.Services.AddScoped<IDalProduct, DalProduct>();
 builder.Services.AddDevExpressBlazor();
 var app = builder.Build();
 if (!app.Environment.IsDevelopment())
