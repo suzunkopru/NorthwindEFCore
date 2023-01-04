bf664b0bd3402d086d8d116dfb78ce7a2588aa16
Süleyman UZUNKÖPRÜ <s-uzunkopru@yandex.com>
2023-01-05 00:32:40 +0300

    .

--- DataAccess/Classes/DalCategory.cs
+++ DataAccess/Classes/DalCategory.cs
@@ -1,9 +1,25 @@
-﻿using DataAccess.Interfaces;
+﻿using AutoMapper;
+using Core.DTOs;
+using DataAccess.Interfaces;
 using Entities.Context;
 using Entities.Models;
 namespace DataAccess.Classes;
 public class DalCategory : EntityRepo<Category>, IDalCategory
 {
-    public DalCategory(NorthwindContext context)
-                            : base(context) { }
+    private readonly IMapper _mapper;
+    private readonly IDalCategory _dalCategory;
+    public DalCategory(NorthwindContext context, IMapper mapper, IDalCategory dalCategory)
+        : base(context)
+    {
+        _mapper = mapper;
+        _dalCategory = dalCategory;
+    }
+    public IQueryable<DtoCategory> GetAll()
+    {
+        var cat = _dalCategory.GetAll();
+        var dtoCat =
+            _mapper.Map<IQueryable<Category>,
+                IQueryable<DtoCategory>>(cat);
+        return dtoCat;
+    }
 }
