25a95e0025d27183765d249323b37b8d163367f2
Süleyman UZUNKÖPRÜ <s-uzunkopru@yandex.com>
2023-01-04 23:22:42 +0300

    .

--- Core/Helper/MappingProfiles.cs
+++ Core/Helper/MappingProfiles.cs
@@ -1,8 +1,6 @@
 ﻿using AutoMapper;
 using Core.DTOs;
 using Entities.Models;
-using DtoOrder = Core.DTOs.DtoOrder;
-
 namespace Core.Helper;
 public class MappingProfiles : Profile
 {
@@ -11,13 +9,12 @@ public class MappingProfiles : Profile
         CreateMap<Category, DtoCategory>().ReverseMap();
         CreateMap<Customer, DtoCustomer>().ReverseMap();
         CreateMap<Employee, DtoEmployee>().ReverseMap();
-        CreateMap<DtoOrder, DtoOrder>().ReverseMap();
-        CreateMap<Product, DtoProduct>().ReverseMap(); 
+        CreateMap<Product, DtoProduct>().ReverseMap();
         CreateMap<Region, DtoRegion>().ReverseMap();
         CreateMap<Shipper, DtoShipper>().ReverseMap();
         CreateMap<Supplier, DtoSupplier>().ReverseMap();
         CreateMap<Territory, DtoTerritory>().ReverseMap()
-        .ForMember(terr => terr.Region,
-            opt => opt.MapFrom(dto => dto.Region));
+            .ForMember(dest => dest.Region,
+                opt => opt.MapFrom(src => src.Region));
     }
 }
