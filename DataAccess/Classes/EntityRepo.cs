4afab94b3cf7379292496c040942061387d10f2c
Süleyman UZUNKÖPRÜ <s-uzunkopru@yandex.com>
2023-01-07 23:32:27 +0300

    .

--- DataAccess/Classes/EntityRepo.cs
+++ DataAccess/Classes/EntityRepo.cs
@@ -2,36 +2,45 @@
 using Entities.Context;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Core.UnitOfWork;
 namespace DataAccess.Classes;
 public class EntityRepo<T> : IEntityRepo<T>
     where T : class, new()
 {
     private readonly NorthwindContext _context;
     private readonly DbSet<T> _dbSet;
+    private readonly IUnitOfWork _unitOfWork;
+    private NorthwindContext context;
     public EntityRepo(NorthwindContext context)
-        => _dbSet = (_context = context).Set<T>();
+    {
+        _unitOfWork = new UnitOfWork(context);
+        _dbSet = (_context = context).Set<T>();
+    }
+
     public IQueryable<T> GetAll()
         => _dbSet.AsNoTracking();
     public IQueryable<T> Where
         (Expression<Func<T, bool>> predicate)
         => _dbSet.AsNoTracking().Where(predicate);
     public async Task<T> GetByIdAsync(int id)
-        => (await _dbSet.FindAsync(id))!;
+        => (await _dbSet.FindAsync(id).ConfigureAwait(false))!;
     public async Task<bool> AnyAsync
         (Expression<Func<T, bool>> predicate)
-        => await _dbSet.AnyAsync(predicate);
+        => await _dbSet.AnyAsync(predicate).ConfigureAwait(false);
     private async Task CUD(T entity, EntityState state)
     {
         _context.Entry(entity).State = state;
-        await _context.SaveChangesAsync();
+        await _unitOfWork.CommitAsync().ConfigureAwait(false);
+        //await _context.SaveChangesAsync();
         _context.Entry(entity).State = EntityState.Detached;
     }
     public async Task AddAsync(T entity)
-        => await Task.FromResult(CUD(entity, EntityState.Added));
+        => await CUD(entity, EntityState.Added)
+                                        .ConfigureAwait(false);
     public async Task AddRangeAsync(IQueryable<T> entities)
     {
-        foreach (var x in entities) await AddAsync(x);
+        foreach (var x in entities) await AddAsync(x)
+                                .ConfigureAwait(false);
     }
     public void Update(T entity)
         => Task.FromResult(CUD(entity, EntityState.Modified));
