607f046f2e052aef966b6411da9e377a00a12839
Süleyman UZUNKÖPRÜ <s-uzunkopru@yandex.com>
2023-01-08 13:57:48 +0300

    .

---
+++ Entities/Paritals/NorthwindContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Entities.Paritals;
+public partial class NorthwindContext : DbContext
+{
+    protected override void OnConfiguring
+        (DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseLazyLoadingProxies(false);
+}
