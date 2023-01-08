5940d01da3b67d60637909584b3cc711b033683d
Süleyman UZUNKÖPRÜ <s-uzunkopru@yandex.com>
2023-01-08 11:19:10 +0300

    TransactionScopeAsyncFlowOption

---
+++ Core/Helper/TranLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Transactions;
+namespace Core.Helper;
+public static class TranLevel
+{
+    public static TransactionScope TranWithNoLock()
+    => new TransactionScope(TransactionScopeOption.Required,
+       new TransactionOptions()
+       {
+           IsolationLevel = IsolationLevel.ReadUncommitted
+       },
+       TransactionScopeAsyncFlowOption.Enabled);
+}
