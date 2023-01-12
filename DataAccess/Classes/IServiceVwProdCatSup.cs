106a002a6c77b0bf0af626145f2f6f4dcf0874d1
Süleyman UZUNKÖPRÜ <s-uzunkopru@yandex.com>
2023-01-12 10:17:52 +0300

    ..

---
+++ DataAccess/Classes/IServiceVwProdCatSup.cs
@@ -0,0 +1,10 @@
+﻿using DataAccess.Interfaces;
+using Entities.Context;
+using Entities.Models;
+namespace DataAccess.Classes;
+public class IServiceVwProdCatSup : 
+        EntityRepo<VwProdCatSup>, IDalVwProdCatSup
+{
+    public IServiceVwProdCatSup(NorthwindContext context)
+        : base(context) { }
+}
