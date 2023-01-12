e1f8761d04e8101733fbb4ea97add1ba86c781ed
Süleyman UZUNKÖPRÜ <s-uzunkopru@yandex.com>
2023-01-12 18:08:01 +0300

    ..

--- Business/Classes/ServiceDtoProductCatName.cs
+++ Business/Classes/ServiceDtoProductCatName.cs
@@ -1,40 +1,17 @@
 ﻿using Business.Interfaces;
 using Core.DTOs;
 using DataAccess.Interfaces;
-using Entities.Context;
 using Entities.Models;
-using Microsoft.EntityFrameworkCore;
 namespace Business.Classes;
 public class ServiceDtoProductCatName
             : Service<Product>, IServiceDtoProductCatName
 {
-    public ServiceDtoProductCatName(IEntityRepo<Product> entityRepo) 
-        : base(entityRepo) { }
-    private readonly NorthwindContext context= new();
+    private readonly IDalDtoProductCatName prodCatName;
+    public ServiceDtoProductCatName(IEntityRepo<Product> entityRepo,
+        IDalDtoProductCatName pProdCatName) : base(entityRepo) 
+                    => prodCatName = pProdCatName;
     public IQueryable<DtoProductCatName> GetProductsCatName()
-        => context.Products.AsQueryable().Join(context.Categories,
-            pr => pr.CategoryId,
-            ct => ct.CategoryId,
-            (pr, ct) =>
-                new DtoProductCatName
-                {
-                    ProductID = pr.ProductId,
-                    ProductName = pr.ProductName,
-                    CategoryName = ct.CategoryName,
-                    UnitsInStock = (short)pr.UnitsInStock
-                });
-    public IQueryable<DtoProductCatName> GetProductsCatName
-        (DbSet<Product> produts, DbSet<Category> categoriest)
-        => from P in produts
-            join C in categoriest
-                on P.CategoryId equals C.CategoryId
-            select new DtoProductCatName
-            {
-                ProductID = P.ProductId,
-                ProductName = P.ProductName,
-                CategoryName = C.CategoryName,
-                UnitsInStock = (short)P.UnitsInStock
-            };
-    public IQueryable<Product> GetProductsByCatergory
-        (int catID) => Where(x => x.CategoryId == catID);
+                    => prodCatName.GetProductsCatName();
+    public IQueryable<Product> GetProductsByCatergory(int prdId)
+                    => prodCatName.GetProductsByCatergory(prdId);
 }
